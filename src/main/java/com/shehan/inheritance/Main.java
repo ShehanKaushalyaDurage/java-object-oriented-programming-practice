bf7485655e898584e8a775227702fb5fd715a3a9
shehan kaushalya <shehankaushalyadurage@gmail.com>
2025-12-28 17:20:14 +0530

    feat: add basic class and object examples

--- src/main/java/com/shehan/inheritance/Main.java
+++ src/main/java/com/shehan/inheritance/Main.java
@@ -1,4 +1,12 @@
 package com.shehan.inheritance;
 
+
 public class Main {
+    public static void main(String[] args) {
+        Box box = new Box(5);
+
+        //Box box1 = new Box(box);
+
+        System.out.println(box.length+" "+box.width+" "+box.height);
+    }
 }
