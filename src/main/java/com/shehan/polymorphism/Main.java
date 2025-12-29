7401176592712e7b02062cb429d6511f3b74cd07
shehan kaushalya <shehankaushalyadurage@gmail.com>
2025-12-29 17:20:42 +0530

    feat: add basic class and object examples

--- src/main/java/com/shehan/polymorphism/Main.java
+++ src/main/java/com/shehan/polymorphism/Main.java
@@ -2,17 +2,33 @@ package com.shehan.polymorphism;
 
 public class Main {
     public static void main(String[] args) {
+        //Normal object creation
+        //No polymorphism here
+        //Reference type = Object type
         Shapes shapes = new Shapes();
         Circle circle = new Circle();
         Square square = new Square();
         Triangle triangle = new Triangle();
 
-        Shapes shapes1 = new Circle();
-        shapes1.area();//Area of Circle
-
+        //Normal object call
         shapes.area();//Area of Shapes
         circle.area();//Area of Circle
         square.area();//Area of Square
         triangle.area();//Area of Triangle
+
+        //Runtime Polymorphism (Dynamic Polymorphism)
+        //polymorphism here
+        //Reference type -> Shapes
+        //Object type -> Circle
+        //A parent reference can refer to a child object
+        Shapes shapes1 = new Circle();
+
+
+        //The Polymorphic Call
+        //Compile time = Does Shapes have area() -> So compilation passes.
+        //Runtime = What is the actual object? > Circle -> Is area() overridden? -> YES
+        shapes1.area();//Area of Circle
+
+
     }
 }
--- src/main/java/com/shehan/polymorphism/Triangle.java
+++ src/main/java/com/shehan/polymorphism/Triangle.java
@@ -2,7 +2,7 @@ package com.shehan.polymorphism;
 
 //Child Classes
 public class Triangle extends Shapes{
-    
+
     //This is method overriding
     void area(){
         System.out.println("Area of Triangle");
