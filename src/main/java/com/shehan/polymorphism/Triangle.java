b4503c02948a54fbd75ea20fcdaeed5b74c9610d
shehan kaushalya <shehankaushalyadurage@gmail.com>
2025-12-29 17:14:56 +0530

    feat: add basic class and object examples

--- src/main/java/com/shehan/polymorphism/Triangle.java
+++ src/main/java/com/shehan/polymorphism/Triangle.java
@@ -2,6 +2,8 @@ package com.shehan.polymorphism;
 
 //Child Classes
 public class Triangle extends Shapes{
+    
+    //This is method overriding
     void area(){
         System.out.println("Area of Triangle");
     }
