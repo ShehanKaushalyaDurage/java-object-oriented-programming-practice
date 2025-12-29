5e375ab3fd1f8006b8aea0073a31ad131e4d5570
shehan kaushalya <shehankaushalyadurage@gmail.com>
2025-12-29 17:38:05 +0530

    feat: add polymorphism examples using method overriding

--- src/main/java/com/shehan/polymorphism/Circle.java
+++ src/main/java/com/shehan/polymorphism/Circle.java
@@ -2,7 +2,17 @@ package com.shehan.polymorphism;
 
 //Child Classes
 public class Circle extends Shapes{
+
+    //this will run when obj of circle is created
+    //hence it is overriding the parent method
+    @Override // this is called annotation
     void area(){
         System.out.println("Area of Circle");
     }
+
+    //Method does not override method from its superclass
+//    @Override
+//    void areaa(){
+//        System.out.println("Area of Circle");
+//    }
 }
