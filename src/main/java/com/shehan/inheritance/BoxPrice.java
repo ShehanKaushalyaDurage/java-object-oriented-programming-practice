7e35df2ff8dfc66ba0e5ef51c23ee65a499a6f73
shehan kaushalya <shehankaushalyadurage@gmail.com>
2025-12-29 14:59:47 +0530

    feat: demonstrate object creation and constructors

--- src/main/java/com/shehan/inheritance/BoxPrice.java
+++ src/main/java/com/shehan/inheritance/BoxPrice.java
@@ -1,4 +1,21 @@
 package com.shehan.inheritance;
 
-public class BoxPrice {
+public class BoxPrice extends BoxWeight{
+
+    double cost;
+
+    public BoxPrice(){
+        super();
+        this.cost = -1;
+    }
+
+    public BoxPrice(BoxPrice other){
+        super(other);
+        this.cost = other.cost;
+    }
+    
+    public BoxPrice(double width, double height, double length, double weight, double cost) {
+        super(width, height, length, weight);
+        this.cost = cost;
+    }
 }
