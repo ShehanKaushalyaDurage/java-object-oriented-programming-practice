34ffc8c343513f6eefdd62e521eb4ce40fb762a3
shehan kaushalya <shehankaushalyadurage@gmail.com>
2026-01-02 13:16:24 +0530

    feat: demonstrate data hiding and validation in encapsulation

--- src/main/java/com/shehan/Encapsulation/Order.java
+++ src/main/java/com/shehan/Encapsulation/Order.java
@@ -3,8 +3,11 @@ package com.shehan.Encapsulation;
 //Encapsulation at System Boundary
 public class Order {
 
+    //State cannot change after creation
+    //Object is immutable
     private final double amount;
 
+    //Validation in constructor -- Object cannot be created in invalid state , No “half-valid” object ever exists
     public Order(double amount) {
         if (amount <= 0) {
             throw new IllegalArgumentException();
