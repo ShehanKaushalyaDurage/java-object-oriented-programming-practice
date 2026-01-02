f0f479002e642483bfacf54c26d02b0eddc922e6
shehan kaushalya <shehankaushalyadurage@gmail.com>
2026-01-02 13:25:53 +0530

    feat: demonstrate data hiding and validation in encapsulation

--- src/main/java/com/shehan/Abstraction/Main.java
+++ src/main/java/com/shehan/Abstraction/Main.java
@@ -8,12 +8,18 @@ public class Main {
     //2. You press accelerator
     //3. You don’t care about engine internals
 
+    //Abstraction means programming against behavior, not implementation, so the caller depends on what an object does, not how it does it.
+
     public static void main(String[] args) {
+
+        //Polymorphism - Runtime binding
+        //Loose Coupling
         Vehicle vehicle = new Car();
         vehicle.start();
 
-        //Open/Closed Principle
+        //Open/Closed Principle -Extend without modify
         //Easy to add new payment methods
+        //Abstraction allows us to add new payment methods without modifying existing code.
         PaymentService service = new PaypalPayment();
         service.pay(20000);
 
@@ -25,6 +31,9 @@ public class Main {
         PaymentService service3 = new PaypalPayment();
         PaymentProcessor processor = new PaymentProcessor(service3);
         processor.process(10000);
+        //High-level module: PaymentProcessor
+        //Low-level modules: PaypalPayment, CreditCardPayment
+        //Both depend on abstraction (PaymentService)
     }
 
 
