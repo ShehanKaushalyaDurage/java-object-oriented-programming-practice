bb9621b97a5c5c8052fc12492a7ca268551a961e
shehan kaushalya <shehankaushalyadurage@gmail.com>
2026-01-02 11:49:23 +0530

    feat: add encapsulation examples using private fields and getters/setters

--- src/main/java/com/shehan/Abstraction/BankAccount.java
+++ src/main/java/com/shehan/Abstraction/BankAccount.java
@@ -2,4 +2,14 @@ package com.shehan.Abstraction;
 
 public class BankAccount {
     public double balance;
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double amount) {
+        if (amount  > 0) {
+            balance += amount;
+        }
+    }
 }
--- src/main/java/com/shehan/Abstraction/Main.java
+++ src/main/java/com/shehan/Abstraction/Main.java
@@ -1,4 +1,12 @@
 package com.shehan.Abstraction;
 
+//Encapsulation -> Protect data
 public class Main {
+    public static void main(String[] args) {
+
+        //BankAccount acc = new BankAccount();
+        //acc.balance = -1000000; // invalid but allowed
+        //System.out.println("The balance is " + acc.balance);
+        //Problem: Anyone can change balance ,No validation
+    }
 }
