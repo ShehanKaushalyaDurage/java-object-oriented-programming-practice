d0e22427b5b76d0f027fe1f5167fccf6849db9e7
shehan kaushalya <shehankaushalyadurage@gmail.com>
2026-01-03 15:33:07 +0530

    feat: add abstraction examples using abstract classes

--- src/main/java/com/shehan/AbstractDemo/Son.java
+++ src/main/java/com/shehan/AbstractDemo/Son.java
@@ -3,11 +3,11 @@ package com.shehan.AbstractDemo;
 public class Son extends Parent {
     @Override
     void career(String name) {
-
+        System.out.println("I'm going to be a " + name);
     }
 
     @Override
     void partner(String name, int age) {
-
+        System.out.println("I'm going to partner " + name + "and she is" + age);
     }
 }
