d5e1bc6994c31931ae42fcfe07694b07d44bb2dc
shehan kaushalya <shehankaushalyadurage@gmail.com>
2026-01-04 20:29:48 +0530

    feat: add Interface examples using classes

--- src/main/java/com/shehan/interfaces/CDPlayer.java
+++ src/main/java/com/shehan/interfaces/CDPlayer.java
@@ -3,11 +3,11 @@ package com.shehan.interfaces;
 public class CDPlayer implements Media {
     @Override
     public void start() {
-        
+        System.out.println("CD player started");
     }
 
     @Override
     public void stop() {
-
+        System.out.println("CD player stopped");
     }
 }
