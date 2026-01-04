6e083b4472525ef87f3f7fce46a9f07350618593
shehan kaushalya <shehankaushalyadurage@gmail.com>
2026-01-04 20:07:19 +0530

    feat: add Interface examples using classes

--- src/main/java/com/shehan/interfaces/Main.java
+++ src/main/java/com/shehan/interfaces/Main.java
@@ -2,15 +2,20 @@ package com.shehan.interfaces;
 
 public class Main {
     public static void main(String[] args) {
+        //Full Access via Concrete Class
         Car car = new Car();
         car.accelerate();
         car.start();
         car.stop();
         car.brake();
 
-        Brake brake = new Car();
-        brake.brake();
-        brake.start();
+        //Car IS-A Brake
+        //Java only allows methods declared in the reference type
+        //This is called Interface-based polymorphism
+
+        //Brake brake = new Car();
+        //brake.brake();
+        //brake.start();
 
         Car car2 = new Engine();
     }
