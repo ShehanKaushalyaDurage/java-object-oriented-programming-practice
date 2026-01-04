2231f2289339d24a74e399e33c34b005081d1c89
shehan kaushalya <shehankaushalyadurage@gmail.com>
2026-01-04 21:18:21 +0530

    feat: add Interface examples using classes

--- src/main/java/com/shehan/interfaces/nested/B.java
+++ src/main/java/com/shehan/interfaces/nested/B.java
@@ -3,6 +3,6 @@ package com.shehan.interfaces.nested;
 public class B implements A.nestedInterface {
     @Override
     public Boolean isOdd(int number) {
-        return number % 2 == 0;
+        return number % 2 != 0;
     }
 }
